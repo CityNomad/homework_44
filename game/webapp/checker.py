a5f9fdd3134439961fdedffbc91ea47135b4517d
valentinaromancheva <romancheva@hotmail.com>
2022-06-21 03:58:19 +0600

    checker was modified

--- game/webapp/checker.py
+++ game/webapp/checker.py
@@ -1,19 +1,43 @@
 class Check:
+    def __int__(self):
+        self.numbers = None
 
-    def checker_list(self, get_nums):
-        if len(get_nums) == 4:
-            if len(get_nums) == len(set(get_nums)):
-                for i in get_nums:
-                    if 0 < i < 10:
-                        return get_nums
-                    else:
-                        raise ValueError
-            else:
-                raise ValueError
+    secret_nums = [2, 4, 6, 8]
+    def checker_list(self, nums):
+        try:
+            numbers = [int(s) for s in nums]
+            if len(numbers) != 4:
+                response_line = "The amount of digits should be equal to 4"
+                return response_line
+            if len(numbers) != len(set(numbers)):
+                response_line = "The value should be unique"
+                return response_line
+            for i in numbers:
+                if i > 9 or i < 1:
+                    response_line = "The digits must be bigger than 0 and less then 10"
+                    return response_line
+            self.numbers = numbers
+
+        except ValueError:
+            response_line = "Please write integers"
+            return response_line
+
+    def get_result(self):
+        bulls = 0
+        cows = 0
+        for i in range(len(self.numbers)):
+            if self.numbers[i] == self.secret_nums[i]:
+                bulls += 1
+            elif self.numbers[i] in self.secret_nums:
+                cows += 1
+        if bulls == 4:
+            response_line = "You win"
+            return response_line
         else:
-            raise ValueError
+            print(bulls)
+            print(cows)
+            response_line = f'You have {bulls} bulls and {cows} cows'
+            return response_line
 
-    def checker_game(self, get_nums, secret_nums):
-        # secret_nums = [2, 4, 6, 8]
-
-        pass
+    def counter(self):
+        return step += 1
--- game/webapp/views.py
+++ game/webapp/views.py
@@ -1,43 +1,34 @@
 from django.shortcuts import render
 from urllib.parse import parse_qs
-from checker import Check
+from webapp.checker import Check
 
 # Create your views here.
 
 def form_view(request):
+    context = {
+    }
+
     if request.method == "GET":
         return render(request, "form.html")
     else:
-        secret_nums = [2, 4, 6, 8]
         context_line = request.POST.get("numbers")
-        context = {
-            'secret_nums' : [2, 4, 6, 8],
-            'bulls': 0,
-            'cows': 0,
-        }
         if context_line:
             context_str = context_line.split()
-            nums = []
-            try:
-                for i in context_str:
-                    i = int(i)
-                    nums.append(i)
-                    try:
-                        nums_checked = Check()
-                        if nums_checked.checker_list(nums):
-                            context['nums'] = nums
-                            return render(request, "form.html", context)
-                    except ValueError:
-                        context['response_line'] = "Please make sure you enter 4 numbers from 1 to 9 and don't repeat them"
-                        return render(request, "form.html", context)
-            except ValueError:
-                context['response_line'] = "Please enter 4 numbers, not text"
+            game = Check()
+            error = game.checker_list(context_str)
+            if error:
+                context["error"] = error
                 return render(request, "form.html", context)
-        else:
-            context['response_line'] = "Empty request is sent"
-            return render(request, "form.html", context)
+            else:
+                result = game.get_result()
+                context["result"] = result
+                context['entered_nums'] = context_line
+                
+                print(context)
+                return render(request, "form.html", context)
 
 
 
-
+def history_view(request):
+    pass
 
